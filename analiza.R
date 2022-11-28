a97cb2416e027d02aafae5637af2320811d3d53e
klaudynka245 <klaudynka245@o2.pl>
2022-11-29 00:26:37 +0100

    Póki działa

--- analiza.R
+++ analiza.R
@@ -1,4 +1,16 @@
+install.packages('stringr')
+
 library(ggplot2)
+library(stringr)
 
 earthquakes = read.csv("Earthquakes.csv")
+
+#place_separated <-str_split(earthquakes$place, " , ", 2)
+#place_separated
+
+test <- vapply(strsplit(earthquakes$place,","), `[`, 2, FUN.VALUE=character(1))
+test
+#ggplot(test) + geom_histogram()
+table(test)
+## dużo danych, przesiejemy je na te z najczęstszymi wystąpieniami obviously 
 
