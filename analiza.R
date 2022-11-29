eec02a7d0593cae6487ee62acbf14f07ac73ec14
klaudynka245 <klaudynka245@o2.pl>
2022-11-29 18:57:01 +0100

    histogram yay

--- analiza.R
+++ analiza.R
@@ -1,7 +1,9 @@
 install.packages('stringr')
+install.packages('reshape')
 
 library(ggplot2)
 library(stringr)
+library(reshape)
 
 earthquakes = read.csv("Earthquakes.csv")
 
@@ -9,8 +11,12 @@ earthquakes = read.csv("Earthquakes.csv")
 #place_separated
 
 test <- vapply(strsplit(earthquakes$place,","), `[`, 2, FUN.VALUE=character(1))
-test
-#ggplot(test) + geom_histogram()
-table(test)
-## dużo danych, przesiejemy je na te z najczęstszymi wystąpieniami obviously 
 
+most_fr_eq <- data.frame(number=rev(sort(table(test)))[1:12])
+colnames(most_fr_eq)[1] <- 'place'
+colnames(most_fr_eq)[2] <- 'frequency'
+most_fr_eq
+
+#par(mar=c(100,300,2,10))
+ggplot(most_fr_eq, aes(x = place, y = frequency)) + geom_col(fill = 'hotpink') + 
+  theme(axis.text.x = element_text(angle = 90)) + geom_text(aes(label = frequency), nudge_y = -20, col='white')
